251929e1da862e62dcd19fde424c45cd69dc863e
vitthal shinde <156592804+vitthalshinde9881@users.noreply.github.com>
2024-10-31 17:40:12 +0000

    JavaScript datatype of function

---
+++ 01- basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number (score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => 1; false => 0 
+
+
+let isLoggedaIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+console.log(typeof stringNumber)
+
+
+// ********************** Operations **************************
+
+let value = 3
+let negValue = -value
+console.log(negValue); 
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+    let gameCounter = 100
+    gameCounter++;
+    console.log(gameCounter);
+
+    // link to study on MND Website 
