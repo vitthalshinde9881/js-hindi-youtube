04744d12aeca53095664fe56e5c2d62f4326951e
vitthal shinde <156592804+vitthalshinde9881@users.noreply.github.com>
2024-10-31 06:23:30 +0000

    datatypes

---
+++ 01- basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat 
+
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // boject
